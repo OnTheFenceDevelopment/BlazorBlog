a1991cb79cba2abae06f626037a993b4ab854af9
Dave Carson <dave@onthefencedevelopment.com>
2022-05-21 16:44:05 +0100

    Updated conversion script
    works but not pretty ;-)

---
+++ src/Utility/wordpress-to-json.ps1
@@ -0,0 +1,71 @@
+$Entries = @()
+$Content = [xml](Get-Content .\dave039splace.wordpress.2022-05-07.000.xml)
+foreach($item in $Content.rss.channel.item)
+{
+	 if ($Item.post_type -eq "post") {
+	
+		$PostPath = $Item.link
+		
+		$OutFileParts = $PostPath.Split("/")
+		$OutFolder = ".\{0}\{1}" -f $OutFileParts[1], $OutFileParts[2]
+		$Title = [string]([System.Web.HttpUtility]::HtmlEncode($item.Title.InnerText))
+		$RawContent = $Item.encoded.InnerText
+		$Content = [string]([System.Web.HttpUtility]::HtmlEncode($RawContent[0]))
+		$CreatedDate = ([datetimeoffset]$Item.PubDate).ToString("o")
+		$Id = $Item.post_id
+
+		# Categories
+		[string]$OutCategories = ""
+		[bool]$added = $false
+		$Cats = $Item.Category
+		foreach($Cat in $Cats)
+		{
+			if ($added)
+			{
+				$OutCategories = $OutCategories + ",`"$($Cat.InnerText)`""
+			} else {
+				$OutCategories = $OutCategories + "`"$($Cat.InnerText)`""
+				$added = $true
+			}
+		}
+
+		# Categories
+		[string]$OutCategories = ""
+		[bool]$added = $false
+		$Cats = $Item.Category
+		foreach($Cat in $Cats)
+		{
+			if ($Cat.InnerText -eq "Uncategorised")
+			{
+				continue;
+			}
+
+			if ($added)
+			{
+				$OutCategories = $OutCategories + ";" + $($Cat.InnerText)
+			} else {
+				$OutCategories = $OutCategories + $($Cat.InnerText)
+				$added = $true
+			}
+		}
+
+		# Generate JSON for data feed
+		[HashTable]$Entry = @{
+			Content = $Content
+			Created = $CreatedDate
+			Id = $Id
+			Title = $Title
+			Permalink = $PostPath
+			Categories = [array]$OutCategories.split(";")
+			Author = [string]"Dave Carson"
+		} # HashTable for Entry
+		
+		$Entries += $Entry
+	
+	}
+
+}
+
+$jsonEntries = $Entries | ConvertTo-Json
+
+Return $jsonEntries
