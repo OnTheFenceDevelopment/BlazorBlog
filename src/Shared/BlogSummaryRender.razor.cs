037fcde10ddcf9369f53694ea4f783963af761cc
Dave Carson <dave@onthefencedevelopment.com>
2022-07-20 20:57:53 +0100

    Added render component to truncate long posts
    Used on Index view when loading multiple posts

---
+++ src/Shared/BlogSummaryRender.razor.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorBlog.Shared
+{
+	using BlazorBlog.Models;
+	using Microsoft.AspNetCore.Components;
+
+	public partial class BlogSummaryRender
+	{
+		[Parameter]
+		public EntryModel? Entry { get; set; }
+	}
+}
