12f515f06950ef63e8afa6b5caa954ebc6da37c3
Dave Carson <dave@onthefencedevelopment.com>
2022-07-20 19:15:14 +0100

    Changed content property to string array to support multiline string

--- src/Models/EntryModel.cs
+++ src/Models/EntryModel.cs
@@ -3,28 +3,20 @@
 	using System;
 	using System.Collections.Generic;
 
-	/// <summary>Entry Model.</summary>
 	public class EntryModel
 	{
-		/// <summary>Gets or sets the blog author.</summary>
-		public string Author { get; set; } = "George Leithead";
+		public string Author { get; set; } = "N/A";
 
-		/// <summary>Gets or sets the categories.</summary>
 		public List<string>? Categories { get; set; }
 
-		/// <summary>Gets or sets the content.</summary>
-		public string Content { get; set; }
+		public string[] Content { get; set; } = new List<string>().ToArray();
 
-		/// <summary>Gets or sets the created date time offset.</summary>
 		public DateTimeOffset Created { get; set; }
 
-		/// <summary>Gets or sets the unique entry identifier.</summary>
 		public int Id { get; set; }
 
-		/// <summary>Gets or sets the PermA link.</summary>
-		public string Permalink { get; set; }
+		public string Permalink { get; set; } = string.Empty;
 
-		/// <summary>Gets or sets the title.</summary>
-		public string Title { get; set; }
+		public string Title { get; set; } = string.Empty;
 	}
 }
